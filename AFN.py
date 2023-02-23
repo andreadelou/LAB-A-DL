af63446e1262bdbe28067e31b80c5d9ae52b5341
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2023-02-23 02:03:26 -0600

    implementar el ?

--- AFN.py
+++ AFN.py
@@ -144,7 +144,30 @@ class AFN():
                     end = c2
                 self.transiciones_splited.append([r1, "ε", r2])
                 self.transiciones_splited.append([r2, "ε", r1])
+            elif i == '?':
+                r1, r2 = stack.pop()
+                counter = counter+1
+                c1 = counter
+                if c1 not in self.estados:
+                    self.estados.append(c1)
+                    counter = counter+1
+                c2 = counter
+                if c2 not in self.estados:
+                    self.estados.append(c2)
+                self.afn_final.append({})
+                self.afn_final.append({})
+                stack.append([c1, r2])
+                self.afn_final[c1]['ε'] = (r1, c2)
+                if start == r1:
+                    start = c1
+                if end == r2:
+                    end = c2
+                self.transiciones_splited.append([r2, "ε", c2])
+                self.transiciones_splited.append([r1, "ε", c2])
+                self.transiciones_splited.append([r1, "ε", r2])
+                self.transiciones_splited.append([c1, "ε", c2])
 
+# En este caso, cuando encontramos el símbolo "?", sacamos un elemento de la pila y creamos dos nuevos estados, c1 y c2. Luego, agregamos una transición de c1 a r1 con ε, y de r2 a c2 con ε. Además, agregamos transiciones de r1 a c2 con ε, y de r1 a r2 con ε. Finalmente, agregamos todas las transiciones a la lista de transiciones splited.
         # asignacion de estados finales e iniciales
         self.e0 = start
         self.ef = end
